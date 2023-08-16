a4e15b0af1c0dcae380bfbb6ad6998ffaddcab79
Eric <ericrivera228@gmail.com>
2023-08-16 17:48:44 -0600

    Printing on submit. Validation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import { TextInput } from './components/TextInput';
 
+interface iFormValue{
+  firstName: string;
+  lastName: string;
+  state: string;
+  city: string;
+  email: string;
+  password: string;
+}
+
+const emptyForm: iFormValue = {
+  firstName: '',
+  lastName: '',
+  state: '',
+  city: '',
+  email: '',
+  password: ''
+};
+
 function App() {
+
+  const [ formValue, setFormValue ] = useState<iFormValue>(emptyForm);
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 
     // Don't need to do the default form submission since we're handling it manually
     event.preventDefault();
 
-    console.log('Hey, listen!');
+    console.log(buildPostString());
+  };
+
+  const onInputValueChange = (fieldName: string, value: string) => {
+    const newFormValue = { ...formValue };
+    newFormValue[fieldName as keyof iFormValue] = value;
+
+    setFormValue(newFormValue);
+  };
+
+  /**
+   * Method that converts the form value into a string representing a JSON post request.
+   */
+  const buildPostString = () => {
+
+    let printString = '{\n';
+
+    // Loop through each of the fields and add it to the print string
+    Object.keys(formValue).forEach(key => {
+      printString += `\t${key}: ${formValue[key as keyof iFormValue]}\n`;
+    });
+
+    printString += '\n}';
+
+    console.log(printString);
+  };
+
+  const isFormValid = () => {
+    Object.keys(formValue).forEach(key => {
+      if(!formValue[key as keyof iFormValue]){
+        return false;
+      }
+    });
+
+    if(!formValue.email.includes('@') || !formValue.email.includes('.')){
+      return false;
+    }
+
+    return true;
   };
 
   return (
@@ -20,14 +78,14 @@ function App() {
           Please enter your information using the form below: 
         </div>
         <div className='form-body'>
-          <TextInput label='First Name' />
-          <TextInput label='Last Name' />
-          <TextInput label='State' />
-          <TextInput label='City' />
-          <TextInput label='Email' />
-          <TextInput label='Password' />
+          <TextInput label='First Name' value={formValue.firstName} onValueChange={(newValue: string) => onInputValueChange('firstName', newValue)}  />
+          <TextInput label='Last Name' value={formValue.lastName} onValueChange={(newValue: string) => onInputValueChange('lastName', newValue)} />
+          <TextInput label='State' value={formValue.state} onValueChange={(newValue: string) => onInputValueChange('state', newValue)} />
+          <TextInput label='City' value={formValue.city} onValueChange={(newValue: string) => onInputValueChange('city', newValue)} />
+          <TextInput label='Email' value={formValue.email} onValueChange={(newValue: string) => onInputValueChange('email', newValue)} />
+          <TextInput label='Password' value={formValue.password} onValueChange={(newValue: string) => onInputValueChange('password', newValue)}/>
         </div>
-        <input type='submit' value='submit' />
+        <input type='submit' value='submit' disabled={!isFormValid()} />
       </form>
     </div>
   );
--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,24 +1,30 @@
 // Third party imports
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 
 // Local imports
 import '../App.css';
 
 interface iTextInputProps{
   label: string;
+  value: string;
+  onValueChange: (newValue: string) => void;
 }
 
-export const TextInput = ({ label }: iTextInputProps) => {
+export const TextInput = ({ label, value, onValueChange }: iTextInputProps) => {
   
   // Convenience method for automatically setting id. Doing this in a production 
   // setting get you into trouble; in my experience it's better to explicity set id. 
   // But for this little app it works just fine :)
   const id = label.replace(' ', '-').toLowerCase();
+
+  const onInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onValueChange(event.target.value);
+  };
   
   return(
     <div>
       <label htmlFor={id} className='form-label'>{label}</label>
-      <input type='text' id={id}></input>
+      <input type='text' value={value} id={id} onChange={onInputChange}></input>
     </div>
   );
 };
